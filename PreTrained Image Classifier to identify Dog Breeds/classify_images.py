e8aceae185dff8999ca34d205664c0cb93de1567
AbelNsukwini <48785213+AbelNsukwini@users.noreply.github.com>
2024-02-19 03:48:26 +0200

    Add files via upload

---
+++ PreTrained Image Classifier to identify Dog Breeds/classify_images.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# */AIPND-revision/intropyproject-classify-pet-images/classify_images.py
+#                                                                             
+# PROGRAMMER: Abel Nsukwini
+# DATE CREATED: 8 July 2023                               
+# REVISED DATE: 
+# PURPOSE: Create a function classify_images that uses the classifier function 
+#          to create the classifier labels and then compares the classifier 
+#          labels to the pet image labels. This function inputs:
+#            -The Image Folder as image_dir within classify_images and function 
+#             and as in_arg.dir for function call within main. 
+#            -The results dictionary as results_dic within classify_images 
+#             function and results for the functin call within main.
+#            -The CNN model architecture as model wihtin classify_images function
+#             and in_arg.arch for the function call within main. 
+#           This function uses the extend function to add items to the list 
+#           that's the 'value' of the results dictionary. You will be adding the
+#           classifier label as the item at index 1 of the list and the comparison 
+#           of the pet and classifier labels as the item at index 2 of the list.
+#
+##
+# Imports classifier function for using CNN to classify images 
+from classifier import classifier 
+
+# TODO 3: Define classify_images function below, specifically replace the None
+#       below by the function definition of the classify_images function. 
+#       Notice that this function doesn't return anything because the 
+#       results_dic dictionary that is passed into the function is a mutable 
+#       data type so no return is needed.
+# 
+def classify_images(images_dir, petlabel_dic, model):
+    """
+    Creates classifier labels with classifier function, compares labels, and 
+    creates a dictionary containing both labels and comparison of them to be
+    returned.
+     PLEASE NOTE: This function uses the classifier() function defined in 
+     classifier.py within this function. The proper use of this function is
+     in test_classifier.py Please refer to this program prior to using the 
+     classifier() function to classify images in this function 
+     Parameters: 
+      images_dir - The (full) path to the folder of images that are to be
+                   classified by pretrained CNN models (string)
+      petlabel_dic - Dictionary that contains the pet image(true) labels
+                     that classify what's in the image, where its' key is the
+                     pet image filename & it's value is pet image label where
+                     label is lowercase with space between each word in label 
+      model - pretrained CNN whose architecture is indicated by this parameter,
+              values must be: resnet alexnet vgg (string)
+     Returns:
+      results_dic - Dictionary with key as image filename and value as a List 
+             (index)idx 0 = pet image label (string)
+                    idx 1 = classifier label (string)
+                    idx 2 = 1/0 (int)   where 1 = match between pet image and 
+                    classifer labels and 0 = no match between labels
+    """
+   
+    
+    results_dic = {}
+    for filename, pet_label in petlabel_dic.items():
+       
+       # Classifies the image using the classifier function and gets the classifier label
+       classifier_label = classifier(images_dir + filename, model)
+       
+       # Converts the classifier label to lowercase and removes any whitespace
+       classifier_label = classifier_label.lower().strip()
+       
+       # Checks if the pet label is in the classifier label using the in operator
+       is_match = int(pet_label in classifier_label)
+       
+       # Adds the result to the results dictionary
+       results_dic[filename] = [pet_label, classifier_label, is_match]
+    
+    return results_dic
+
+
+    
+
+    
